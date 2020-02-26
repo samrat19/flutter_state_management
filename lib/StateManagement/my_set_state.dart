0b8a0dff8e524faef34016e7c10f1cf77c14f70f
Atri Das <babinsamrat123@gmail.com>
2020-02-26 23:37:13 +0530

    implements setState

--- lib/StateManagement/my_set_state.dart
+++ lib/StateManagement/my_set_state.dart
@@ -6,8 +6,32 @@ class MySetState extends StatefulWidget {
 }
 
 class _MySetStateState extends State<MySetState> {
+
+  int digit = 0;
+  void addOne(){
+    setState(() {
+      digit = digit + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Set State'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: addOne,
+        child: Icon(Icons.add),
+      ),
+      body: Center(
+        child: Text(
+          digit.toString(),
+          style: TextStyle(
+            fontSize: 90.0,
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_state_management/StateManagement/my_set_state.dart';
 
 main() => runApp(MaterialApp(
+  initialRoute: 'set-state',
   routes: {
-
+    'set-state': (_) => MySetState(),
   },
 ),);
