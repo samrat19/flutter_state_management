7c216cd3289876da1ada4574ff549bc82ffeacad
Atri Das <babinsamrat123@gmail.com>
2020-02-27 20:20:08 +0530

    implements rx BloC

--- lib/StateManagement/my_rx_dart.dart
+++ lib/StateManagement/my_rx_dart.dart
@@ -1,17 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
 
-class MyRxDart extends StatelessWidget {
+class MyRxDart extends StatefulWidget {
+  @override
+  _MyRxDartState createState() => _MyRxDartState();
+}
+
+class _MyRxDartState extends State<MyRxDart> {
+  CounterBloc _counterBloc = CounterBloc(initialCount: 0);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Rx BLoC'),
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+        child: StreamBuilder(
+          stream: _counterBloc.counterObservable,
+          builder: (context, AsyncSnapshot<int> snapshot) => Text(
+            snapshot.data.toString(),
+            style: TextStyle(
+              fontSize: 70.0,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
 
+class CounterBloc {
+  int initialCount = 0;
+  BehaviorSubject<int> _subjectCounter;
 
-class IncrementOne{
+  CounterBloc({this.initialCount}) {
+    _subjectCounter = BehaviorSubject<int>.seeded(
+        this.initialCount); //initializes the subject with element already
+  }
 
-  BehaviorSubject _counter =  BehaviorSubject.seeded(0);
+  Stream<int> get counterObservable => _subjectCounter.stream;
 
-  Stream get stream => _counter.stream;
+  void increment() {
+    initialCount++;
+    _subjectCounter.sink.add(initialCount);
+  }
+
+//  void decrement(){
+//    initialCount--;
+//    _subjectCounter.sink.add(initialCount);
+//  }
+
+  void dispose() {
+    _subjectCounter.close();
+  }
 }
